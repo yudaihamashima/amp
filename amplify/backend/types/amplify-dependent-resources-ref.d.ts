2ecd88dc493f016311142ebac8546cb07f9258e8
yudaihamashima <newlovenewworld2@gmail.com>
2022-07-13 14:12:48 +0900

    fin

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3aa0a8cee": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
